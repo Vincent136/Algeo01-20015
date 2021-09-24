e7f03438dd696c1f37ab6a08bd0e8dd7c9d65b3d
Raven <16520022@std.stei.ac.id>
2021-09-24 22:17:59 +0700

    Fungsi I/O ke file

---
+++ src/InputOutput.java
@@ -0,0 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter; 
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class InputOutput {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    //Membaca matrixaugmented dari file dengan nama berdasarkan input
+    //Input diasumsikan benar
+    //Asumsi file ada isinya dan dalam bentuk matriks double:
+    //Ex:   0 0 0.1
+    //      0.3 2 4
+    //      8 4 5
+    // Returnnya matrix double
+    // Kalau gagal, return matrix size 1x1 isinya -999
+    public static double[][] ReadMatrixFromFile() {
+        System.out.print("Masukan nama file: ");        //Nama File
+        String File = scan.next();
+
+        try { 
+            Scanner  PanjangFile = new Scanner(new File("../test/"+File));
+            String[] PanjangBaris = PanjangFile.nextLine().split(" ");
+
+            int count =1;
+            while (PanjangFile.hasNextLine()) {
+                count++;
+                PanjangFile.nextLine();
+              }
+            PanjangFile.close();
+
+            int row = count;
+            int column = PanjangBaris.length;
+            double[][] Matriks = new double[row][column];
+
+            Scanner IsiFile = new Scanner(new File("../test/"+File));
+    
+            for (int r = 0; r < row; r++) {
+                for (int c = 0; c < column; c++) {
+                    Matriks[r][c] = IsiFile.nextDouble();
+                }
+            }
+            return Matriks;
+
+        } catch (FileNotFoundException e) {
+            System.out.print("File Tidak Ditemukan/Terjadi Error");
+            double[][] MatriksInvalid = new double[0][0];
+            MatriksInvalid[0][0] = -999;
+            return MatriksInvalid;
+        }
+    }
+
+    //Menulis (rewrite/create) MatriksofDouble ke file kosong dengan nama berdasarkan input
+    //Input diasumsikan benar
+    public static void WriteMatrixToFile(double[][] Matriks){
+        try {
+            System.out.print("Masukan nama file: ");    //Nama File
+            String File = scan.next();
+            
+            String Isifile ="";
+            for (int i = 0; i < Matriks.length; i++) {
+                for (int j = 0; j < Matriks[0].length; j++) {
+                    Isifile += Double.toString(Matriks[i][j]);
+                    if(j != Matriks[0].length){
+                        Isifile+=" ";
+                    }
+                }
+                if(i!=Matriks.length){
+                    Isifile+="\n";
+                }
+            }
+            FileWriter myWriter = new FileWriter("../test/"+File);
+            myWriter.write(Isifile);
+            myWriter.close();
+            System.out.println("File sudah ditulis");
+
+        }catch (IOException e) {
+            System.out.println("Terjadi Error");
+            e.printStackTrace();
+          }
+    }
+
+    //Menulis (rewrite/create) ArrayofString ke file kosong dengan nama berdasarkan input
+    //Input diasumsikan benar
+    public static void WriteToFile(String[] ArrayIsiFile){
+        try {
+            System.out.print("Masukan nama file: ");    //Nama File
+            String File = scan.next();
+            
+            String Isifile ="";
+            for (int i = 0; i < ArrayIsiFile.length; i++) {
+                Isifile += ArrayIsiFile[i];
+                if (i != ArrayIsiFile.length){
+                    Isifile +="\n";
+                }
+            }
+
+            FileWriter myWriter = new FileWriter("../test/"+File);
+            myWriter.write(Isifile);
+            myWriter.close();
+
+            System.out.println("File sudah ditulis");
+
+        }catch (IOException e) {
+            System.out.println("Terjadi Error");
+            e.printStackTrace();
+          }
+    }
+}
